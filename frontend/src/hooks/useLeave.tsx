3cb13f93451809b84bbb5552aa912212bb060d0c
notepad101 <jcawesomely@gmail.com>
2026-08-13 20:37:17 +0800

    not done yet

--- frontend/src/hooks/useLeave.tsx
+++ frontend/src/hooks/useLeave.tsx
@@ -1,4 +1,4 @@
-import { useQueryClient,useQuery,useMutation } from "@tanstack/react-query";
+import { useQueryClient,useQuery,useMutation, Mutation } from "@tanstack/react-query";
 import apiClient from "@/lib/api-client";
 import type { LeaveRequest,LeaveApproval,LeaveCredits,LeaveStatus,LeaveStatusName,LeaveType } from "@/types/attendance";
 
@@ -29,6 +29,48 @@ export function useLeaveStatues(){
         queryKey:LEAVE_REQUEST_KEY,
         queryFn:async () => {
             const {data} = await apiClient.get<LeaveStatus[]>('attendance/leave-status/')
+            return data
         }
     })
+}
+
+
+export function useCreateLeaveRequest() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async(payload: Partial<LeaveRequest>) => {
+            const {data} = await apiClient.post<LeaveRequest>('attendance/leave-request/',payload)
+            return data
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey:LEAVE_REQUEST_KEY})
+        },
+    });
+}
+
+
+export function useUpdateLeaveRequest() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async({id,payload} : {id:number,payload:Partial<LeaveRequest>}) => {
+            const {data} = await apiClient.put<LeaveRequest>(`attendance/leave-request/${id}/`,payload)
+            return data
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey:LEAVE_REQUEST_KEY})
+        },
+    });
+}
+
+export function useDeleteLeaveRequest() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async(id:number) => {
+            const {data} = await apiClient.delete<LeaveRequest>(`attendance/leave-request/${id}`)
+            return data
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey:LEAVE_REQUEST_KEY})
+        }
+    });
 }
